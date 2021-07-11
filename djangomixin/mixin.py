026d276ea983fe821c642262428cdc8bb63401e2
Nj Nafir <mdanjenafir24434@gmail.com>
2021-07-11 12:07:33 +0600

    Simple mixin code documentation

--- djangomixin/mixin.py
+++ djangomixin/mixin.py
@@ -1,7 +1,23 @@
 import random, string, os
 
+"""
+Some handy snippets for django
+"""
 
 def bootstrap_visible_fields(visible_fields):
+    """
+        It's a good snippet to upgrade your django form fields classes for bootstrap
+        In your form class defination in the __init__ file let's add this line
+        after import it
+
+        from djangomixin import bootstrap_visible_fields
+
+        class MyForm(forms.ModelForm):
+            def __init__(self, *args, **kwargs):
+                bootstrap_visible_fields(self.visible_fields)
+                super(MyForm, self).__init__(*args, **kwargs)
+
+    """
     for visible in visible_fields():
         widget = visible.field.widget
         class_name = widget.__class__.__name__
@@ -15,6 +31,10 @@ def bootstrap_visible_fields(visible_fields):
 
 
 def random_string(lenght=10):
+    """
+        Sometimes we need to generate random string for id or slug
+        Handy snippet to give a random lengthy string
+    """
 	chars = string.ascii_lowercase + string.digits
 	plus = ''.join(random.choice(chars) for _ in range(lenght))
 	dig = range(0,9)
@@ -23,4 +43,7 @@ def random_string(lenght=10):
 
 
 def filename(file):
+    """
+        Find file actual name by it's path
+    """
 	return os.path.basename(file)
