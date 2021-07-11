f5811cc30a0fc27e33c7368ef3f6314ab27ebf0d
Nj Nafir <mdanjenafir24434@gmail.com>
2021-07-11 12:06:48 +0600

    Class mixin code documentation

--- djangomixin/class_mixin.py
+++ djangomixin/class_mixin.py
@@ -1,7 +1,14 @@
 from django.utils.http import is_safe_url
 
+"""
+class mixin stands for django class based view mixin.
+It's can be inherited in your view defination to extend functionality.
+"""
 
 class RequestAttachMixin(object):
+	"""
+		Mixin to attach session request object to your class view
+	"""
 	def get_form_kwargs(self):
 		kwargs = super(RequestAttachMixin, self).get_form_kwargs()
 		kwargs['request'] = self.request
@@ -9,6 +16,10 @@ class RequestAttachMixin(object):
 
 
 class NextUrlMixin(object):
+	"""
+		Mixin to attach method for ?next=anything to your class view
+		Retrive the next url by calling into get_next_url method
+	"""
 	default_path = '/'
 	def get_next_url(self):
 		request = self.request
